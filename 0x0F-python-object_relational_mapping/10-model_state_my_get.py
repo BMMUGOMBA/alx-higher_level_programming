5e32773eeb43ded8ce349c1450aa6f7876b41666
BMMUGOMBA <bmmugomba@gmail.com>
2023-08-26 04:05:40 +0200

    second deadline edit

--- 0x0F-python-object_relational_mapping/10-model_state_my_get.py
+++ 0x0F-python-object_relational_mapping/10-model_state_my_get.py
@@ -1,42 +1,30 @@
 #!/usr/bin/python3
 """
-Prints the State object with the name passed as argument from
-the database hbtn_0e_6_usa
+This script prints the first State object
+from the database `hbtn_0e_6_usa`.
 """
 
-import sys
-from model_state import Base, State
+from sys import argv
+from model_state import State, Base
 from sqlalchemy import create_engine
 from sqlalchemy.orm import sessionmaker
 
 if __name__ == "__main__":
-    engine = create_engine('mysql+mysqldb://{}:{}@localhost/{}'.
-                           format(sys.argv[1], sys.argv[2],
-                                  sys.argv[3], sys.argv[4]),
-                           pool_pre_ping=True)
+    """
+    Access to the database and get a state
+    from the database.
+    """
 
-    # Initialize engine
-    Base.metadata.create_all(engine)
+    db_url = "mysql+mysqldb://{}:{}@localhost:3306/{}".format(
+        argv[1], argv[2], argv[3])
 
-    # Initialize session
+    engine = create_engine(db_url)
     Session = sessionmaker(bind=engine)
+
     session = Session()
-    query = session.query(State).all()
 
-    # Create all name for states
-    list_state = []
-    for state in query:
-        list_state.append(state.name)
-
-    # Query
-    query = session.query(State).\
-        filter(State.name == "{}".format(sys.argv[4], )).first()
-
-    # Conditions
-    if sys.argv[4] not in list_state:
-        print("Not found")
+    state = session.query(State).filter(State.name == argv[4]).first()
+    if state is not None:
+        print('{0}'.format(state.id))
     else:
-        print(query.id)
-
-    # Close session
-    session.close()
+        print("Not found")
--- 0x0F-python-object_relational_mapping/8-model_state_fetch_first.py
+++ 0x0F-python-object_relational_mapping/8-model_state_fetch_first.py
@@ -1,34 +1,30 @@
 #!/usr/bin/python3
 """
-Prints the first State object from the
-database hbtn_0e_6_usa
+This script prints the first State object
+from the database `hbtn_0e_6_usa`.
 """
 
-import sys
-from model_state import Base, State
+from sys import argv
+from model_state import State, Base
 from sqlalchemy import create_engine
 from sqlalchemy.orm import sessionmaker
 
 if __name__ == "__main__":
-    engine = create_engine('mysql+mysqldb://{}:{}@localhost/{}'.
-                           format(sys.argv[1], sys.argv[2], sys.argv[3]),
-                           pool_pre_ping=True)
+    """
+    Access to the database and get a state
+    from the database.
+    """
 
-    # Initialize engine
-    Base.metadata.create_all(engine)
+    db_url = "mysql+mysqldb://{}:{}@localhost:3306/{}".format(
+        argv[1], argv[2], argv[3])
 
-    # Initialize session
+    engine = create_engine(db_url)
     Session = sessionmaker(bind=engine)
+
     session = Session()
 
-    # Query
-    query = session.query(State).first()
-
-    # Print query
-    if query is None:
-        print("Nothing")
+    state = session.query(State).order_by(State.id).first()
+    if state is not None:
+        print('{0}: {1}'.format(state.id, state.name))
     else:
-        print("{}: {}".format(query.id, query.name))
-
-    # Close session
-    session.close()
+        print("Nothing")
