3cd58f48e667c0819077ab4fdea138a8f2a95b9e
BMMUGOMBA <bmmugomba@gmail.com>
2023-08-26 04:04:44 +0200

    second deadline 567

--- 0x0F-python-object_relational_mapping/7-model_state_fetch_all.py
+++ 0x0F-python-object_relational_mapping/7-model_state_fetch_all.py
@@ -1,28 +1,27 @@
 #!/usr/bin/python3
-"""lists all State objects from the database hbtn_0e_6_usa"""
+"""
+This script lists all State objects
+from the database `hbtn_0e_6_usa`.
+"""
 
-import sys
-from model_state import Base, State
+from sys import argv
+from model_state import State, Base
 from sqlalchemy import create_engine
 from sqlalchemy.orm import sessionmaker
 
 if __name__ == "__main__":
-    engine = create_engine('mysql+mysqldb://{}:{}@localhost/{}'.
-                           format(sys.argv[1], sys.argv[2], sys.argv[3]),
-                           pool_pre_ping=True)
-    # Initialize engine
-    Base.metadata.create_all(engine)
+    """
+    Access to the database and get the states
+    from the database.
+    """
 
-    # Initialize session
+    db_url = "mysql+mysqldb://{}:{}@localhost:3306/{}".format(
+        argv[1], argv[2], argv[3])
+
+    engine = create_engine(db_url)
     Session = sessionmaker(bind=engine)
+
     session = Session()
 
-    # Query
-    query = session.query(State).order_by(State.id).all()
-
-    # Print query
-    for record in query:
-        print("{}: {}".format(record.id, record.name))
-
-    # Close session
-    session.close()
+    for instance in session.query(State).order_by(State.id):
+        print('{0}: {1}'.format(instance.id, instance.name))
