0c61b2d2251f717abd5250b748ccb939cea62dd1
BMMUGOMBA <bmmugomba@gmail.com>
2023-09-02 00:09:15 +0200

    123

--- 0x11-python-network_1/5-hbtn_header.py
+++ 0x11-python-network_1/5-hbtn_header.py
@@ -1,13 +1,13 @@
 #!/usr/bin/python3
-"""Displays the X-Request-Id header variable of a request to a given URL.
-Usage: ./5-hbtn_header.py <URL>
+"""Retrieve header with requests module
 """
-import sys
+
 import requests
+from sys import argv
 
 
-if __name__ == "__main__":
-    url = sys.argv[1]
-
+if __name__ == '__main__':
+    url = argv[1]
     r = requests.get(url)
-    print(r.headers.get("X-Request-Id"))
+    r = r.headers
+    print(r.get('X-Request-Id'))
