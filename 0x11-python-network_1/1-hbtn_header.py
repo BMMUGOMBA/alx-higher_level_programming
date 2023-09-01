d1b491437027ef8d34560a1a27d7f1e79f51ac5e
BMMUGOMBA <bmmugomba@gmail.com>
2023-09-02 00:06:30 +0200

    adding my header

--- 0x11-python-network_1/1-hbtn_header.py
+++ 0x11-python-network_1/1-hbtn_header.py
@@ -1,14 +1,12 @@
 #!/usr/bin/python3
-"""Displays the X-Request-Id header variable of a request to a given URL.
-Usage: ./1-hbtn_header.py <URL>
+"""Request and parse header
 """
-import sys
-import urllib.request
 
+import urllib.request
+from sys import argv
 
 if __name__ == "__main__":
-    url = sys.argv[1]
-
-    request = urllib.request.Request(url)
-    with urllib.request.urlopen(request) as response:
-        print(dict(response.headers).get("X-Request-Id"))
+    with urllib.request.urlopen(argv[1]) as response:
+        for header in response.getheaders():
+            if header[0] == 'X-Request-Id':
+                print(header[1])
