06e6b35fb8e217b6cd696456e28dc40ba85e870c
BMMUGOMBA <bmmugomba@gmail.com>
2023-09-02 00:09:55 +0200

    email

--- 0x11-python-network_1/6-post_email.py
+++ 0x11-python-network_1/6-post_email.py
@@ -1,15 +1,14 @@
 #!/usr/bin/python3
-"""Sends a POST request to a given URL with a given email.
-Usage: ./6-post_email.py <URL> <email>
-  - Displays the body of the response.
+"""Make a POST request
 """
-import sys
+
 import requests
+from sys import argv
 
+if __name__ == '__main__':
 
-if __name__ == "__main__":
-    url = sys.argv[1]
-    value = {"email": sys.argv[2]}
+    url = argv[1]
+    email = argv[2]
 
-    r = requests.post(url, data=value)
+    r = requests.post(url, data={'email': email})
     print(r.text)
